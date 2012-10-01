23f9b849c6bc10d05d60a619d3ce5f881dee889e
David Hu <davidhu91@gmail.com>
2012-10-01 16:34:36 -0400

    set up blog on new machine (old one died for mysterious reason)

---
+++ aws_config/setup.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# This sets up my blog.
+#
+# Idempotent.
+#
+# This can be run like
+#
+# $ cat setup.sh | ssh <hostname of EC2 machine> sh
+#
+# But first, create a user "david" by running
+#
+# ./create_role_account.sh rmc | ssh -i KEYFILE ubuntu@IP_ADDRESS sh
+#
+
+
+# Bail on any errors
+set -e
+
+CONFIG_DIR=$HOME/blog/aws_setup
+
+cd $HOME
+
+sudo apt-get update
+
+echo "Installing developer tools"
+sudo apt-get install -y curl
+sudo apt-get install -y python-pip
+sudo apt-get install -y build-essential python-dev
+sudo apt-get install -y git
+sudo apt-get install -y unzip
+sudo apt-get install -y ruby rubygems
+sudo REALLY_GEM_UPDATE_SYSTEM=1 gem update --system
+
+echo "Copying dotfiles"
+git clone https://github.com/divad12/dotfiles.git || ( cd dotfiles && git pull )
+( cd dotfiles && ./symlink.sh )
+
+echo "Setting up blog"
+git clone https://github.com/divad12/blog.git || ( cd blog && git pull )
+sudo gem install jekyll
+sudo gem install rdiscount
+sudo apt-get install -y blahtexml
+jekyll
+
+echo "Installing nginx"
+sudo apt-get install -y nginx
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo ln -sfnv $CONFIG_DIR/etc/nginx/sites-available/blog \
+  /etc/nginx/sites-available/blog
+sudo ln -sfnv /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/blog
+sudo service nginx restart
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# TODO: give --delete option to rsync
-jekyll && rsync -avz _site/ ec2-user@david-hu.com:~/www
+# Updates my blog on my server.
+# Can be run either directly on the machine, or by running
+#
+# $ cat deploy.sh | ssh david sh
+#
+
+set -e  # Bail on errors
+
+cd $HOME/blog
+
+git pull
+
+echo "Running Jekyll"
+jekyll
